25b95a7ade845fa41ab92097b687f4a6589ae5a9
Elisabeth Rosentel <ellierosentel@gmail.com>
2018-09-09 22:51:54 -0700

    Added print methods and shuffle

--- Deck.py
+++ Deck.py
@@ -1,19 +1,26 @@
 import random
+import Cards
 
 
 class Deck:
 
     cards = 52
     faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-    suits = ['hearts', 'spades', 'clubs', 'diamonds']
-    values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'Jack': 10,
-              'Queen': 10, 'King': 10, 'Ace': 11}
+    suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
 
     def __init__(self):
         self.cards = []
-        for face in faces:
-            for suit in suits:
-                self.cards.append(self, Card(suit, face))
+        for face in Deck.faces:
+            for suit in Deck.suits:
+                self.cards.append(self, Cards.Cards(suit, face))
 
     def shuffle(self):
-        pass
+        random.shuffle(self.cards)
+
+    def __str__(self):
+        for card in self.cards:
+            print(card)
+
+    def deal(self):
+        return self.cards.pop(self)
+
